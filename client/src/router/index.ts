c2808ab4f8de3e6880da9283f71b2a88c341dab6
Lukas Januskevicius <lukasjanuskevicius@MacBookPro.lan>
2025-02-19 19:29:24 +0200

    client: Ai generated audio playback manual testing

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import Authenticated from '@/views/Authenticated.vue'
 import HomeView from '@/views/HomeView.vue'
+import PlaybackView from '@/views/PlaybackView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/authenticated',
       name: 'Authenticated',
       component: Authenticated,
+    },
+    {
+      path: '/playback',
+      name: 'Playback',
+      component: PlaybackView,
     },
   ],
 })
---
+++ client/src/views/PlaybackView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { trpc } from '@/trpc'
+
+const songUrl = ref(
+  'https://files.topmediai.com/aimusic/api/6ad2dc9a-ee46-4d83-8d1f-682efe46c1f9-audio.mp3'
+)
+const waiting = ref(false)
+const getSong = async () => {
+  waiting.value = true
+  const songs = await trpc.topmediai.fetchSongs.mutate({
+    prompt:
+      'Generate a high-energy electronic track with fast beats (140 BPM), deep bass, and uplifting synths. Inspired by progressive house, perfect for intense workouts and confident, extroverted individuals.',
+    lyric:
+      'Verse 1 🔥 Push it to the limit, feel the fire rise No looking back now, see it in my eyes Heartbeat racing, I’m breaking throughStronger than ever, nothing I can’t do Pre-Chorus ⚡ Run, jump, sweat, let it all igniteTurn up the volume, feel the bass take flight One more rep, one more mile Keep it going, make it worthwhile Chorus 🚀 Rise up, no stopping now Feel the power, shake the ground Lights flash, we own the night Keep on moving, reach new heights Drop (Instrumental Build-Up)',
+    title: 'Boss rhymes',
+  })
+
+  songUrl.value = songs[0].audio_file
+  waiting.value = false
+}
+</script>
+
+<template>
+  <div v-if="waiting">Waiting for song</div>
+  <div v-if="songUrl.length > 1">
+    <img src="" />
+    <audio controls>
+      <source :src="songUrl" type="audio/mp3" />
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+  <div v-else><button @click="getSong">Get me a song!</button></div>
+</template>
+
+<style scoped></style>
