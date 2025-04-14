62a72c9bb3f33aa6621ac48b16d3ac22de23ed2a
Lukas Januskevicius <lukasjanuskevicius@MacBookPro.lan>
2025-04-14 15:55:46 +0300

    small fix

--- client/src/trpc/index.ts
+++ client/src/trpc/index.ts
@@ -3,15 +3,16 @@ import type { AppRouter } from '@server/shared/trpc'
 import { getStoredAccessToken } from '@/utils/auth'
 import { apiBase } from '@/config'
 
-export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [
-    httpBatchLink({
-      url: apiBase,
-      headers: () => {
-        const token = getStoredAccessToken(localStorage)
-        if (token) return { authorization: `Bearer ${token}` }
-        return {}
-      },
-    }),
-  ],
-})
+export const trpc: ReturnType<typeof createTRPCProxyClient<AppRouter>> =
+  createTRPCProxyClient<AppRouter>({
+    links: [
+      httpBatchLink({
+        url: apiBase,
+        headers: () => {
+          const token = getStoredAccessToken(localStorage)
+          if (token) return { authorization: `Bearer ${token}` }
+          return {}
+        },
+      }),
+    ],
+  })
