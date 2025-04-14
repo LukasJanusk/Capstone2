7705fbfedb951a505c846f9e84b38ae51c765093
Lukas Januskevicius <lukasjanuskevicius@MacBookPro.lan>
2025-04-14 15:19:25 +0300

    fix: config for proxy

--- client/e2e/singup.spec.ts
+++ client/e2e/singup.spec.ts
@@ -8,11 +8,11 @@ test.describe('HomeView', () => {
   test('navigates to singup page', async ({ page }) => {
     await page.goto('/')
     await page.getByRole('button', { name: 'Sign up' }).click()
-
     expect(page.getByText('First name*')).toBeVisible()
     expect(page.getByText('Last name*')).toBeVisible()
     expect(page.getByText('Password*')).toBeVisible()
     expect(page.getByText('Traits*')).toBeVisible()
-    expect(page.getByRole('button', { name: 'Sign Up' })).toBeVisible()
+
+    expect(page.getByRole('button', { name: 'Sign Up', exact: true })).toBeVisible()
   })
 })
--- server/src/controllers/strava/getClientId.ts
+++ server/src/controllers/strava/getClientId.ts
@@ -1,8 +1,4 @@
-import provideRepos from '@server/trpc/provideRepos'
-import { userRepository } from '@server/repositories/userRepository'
 import { publicProcedure } from '@server/trpc'
 import config from '@server/config'
 
-export default publicProcedure
-  .use(provideRepos({ userRepository }))
-  .query(async () => config.stravaClientId)
+export default publicProcedure.query(async () => config.stravaClientId)
